3233ddcf7b09af95e6f8aad339f29d08e703a978
Shrey Shah <shreybshah.97.ss@gmail.com>
2022-10-16 01:33:22 -0700

    Code refactoring

--- basic_3.py
+++ basic_3.py
@@ -1,33 +1,52 @@
+from ctypes import alignment
 import sys
 
-def generate_string(input_file):
+def read_file(input_file):
+    """
+    Reads the input file and returns the lines read.
+    """
+    with open(input_file, 'r') as f:
+        lines = f.readlines()
+        f.close()
+    return lines
+    
+    
+def generate_strings(lines):
     """
     Generates and returns the strings generated from the input file.
     """
-    with open(input_file, 'r') as f:
-        count=0
-        while True:
-            line = f.readline().strip()
-            if not line:
-                break
-            if(not line.isnumeric()):
-                if count!=0:
-                    string1 = base
-                    if(len(string1)!=(base_length*int(2**count))):
-                        print("Length of generated string is not 2^j")
-                    count=0
-                base = line
-                base_length = len(line)
-            else:
-                count+=1
-                breakpt = int(line)+1
-                base = base[:breakpt] + base + base[breakpt:]
-            
-    string2 = base
-    if(len(string2)!=(base_length*int(2**count))):
-        print("Length of generated string is not 2^k")
+    operation_counts = []
+    base_lengths = []
+    generated_strings = []
     count=0
-    return string1, string2
+
+    for line in lines:
+        line = line.strip()
+        if(not line.isnumeric()):
+            if count != 0:
+                generated_strings.append(base)
+                operation_counts.append(count)
+                count = 0
+            base = line
+            base_lengths.append(len(line))
+        else:
+            count += 1
+            insert_at = int(line) + 1
+            base = base[:insert_at] + base + base[insert_at:]
+            
+    generated_strings.append(base)
+    operation_counts.append(count)
+    return generated_strings, base_lengths, operation_counts
+
+def validate_strings(generated_strings, base_lengths, operation_counts):
+    """
+    Validates string generated are of optimal length
+    """
+    inputs = list(zip(generated_strings, base_lengths, operation_counts))
+    for input in inputs:
+        if(len(input[0]) != (input[1] * int(2 ** input[2]))):
+            print("Length of generated string is not 2^j")
+            return -1
 
 def initialize_variables():
     """
@@ -37,35 +56,57 @@ def initialize_variables():
     DELTA = 30
     ALPHA = [[0,110,48,94],[110,0,118,48],[48,118,0,110],[94,48,110,0]]
 
-def calculate_cost(string1, string2):
+def calculate_cost(generated_strings):
     """
     Calculates the minimum cost alignment for the sequences string1 and string2
     """
-    print(string1+"\n"+string2)
-    m = len(string1)
-    n = len(string2)
+    string1, string2 = generated_strings
+    string1_len = len(string1)
+    string2_len = len(string2)
     dict = {"A":0, "C":1, "G":2, "T":3}
-    dp = [[0] * (m+1) for _ in range(0, n+1)]
-    for i in range(1,m+1):
-        dp[i][0] = i*DELTA
-    for i in range(1,n+1):
-        dp[0][i] = i*DELTA
+    dp = [[0] * (string1_len+1) for _ in range(0, string2_len+1)]
 
-    for i in range(1,m+1):
-        for j in range(1,n+1):
-            dp[i][j] = min(dp[i-1][j-1]+ALPHA[dict[string1[i-1]]][dict[string2[j-1]]], DELTA+dp[i-1][j], DELTA+dp[i][j-1])
+    for i in range(1, string1_len+1):
+        dp[i][0] = i * DELTA
+    for i in range(1, string2_len+1):
+        dp[0][i] = i * DELTA
 
-    print(dp)
-    return dp[m][n]
+    #TODO: Check if i+1 instead of i makes more sense
+    for i in range(1, string1_len+1):
+        for j in range(1, string2_len+1):
+            dp[i][j] = min(
+                dp[i-1][j-1] + ALPHA[dict[string1[i-1]]][dict[string2[j-1]]], 
+                DELTA + dp[i-1][j],
+                DELTA + dp[i][j-1])
 
+    print(dp[string1_len][string2_len])
+    return dp
+
+def calculate_alignment(generated_strings, dp):
+    """
+    Returns the aligned string for string1 and string2
+    """
+    string1, string2 = generated_strings
+    string1_len = len(string1)
+    string2_len = len(string2)
+    dict = {"A":0, "C":1, "G":2, "T":3}
+    alignment_string_1 = ""
+    alignment_string_1 = ""
+    for i in range(1, string1_len + 1):
+        for j in range(1, string2_len + 1):
+            break
+    return
 
 def main():
-    input_file= sys.argv[1] 
-    output_file= sys.argv[2]
+    input_file_path = sys.argv[1] 
+    output_file_path = sys.argv[2]
     initialize_variables()
-    string1, string2 = generate_string(input_file)
-    print(string1+"\n"+string2)
-    calculate_cost(string1,string2)
+    lines = read_file(input_file_path)
+    generated_strings, base_lengths, operation_counts = generate_strings(lines)
+    validate_strings(generated_strings, base_lengths, operation_counts)
+    print(generated_strings[0]+"\n"+generated_strings[1])
+    dp = calculate_cost(generated_strings)
+    # calculate_alignment(generated_strings, dp)
     return
 
 if __name__ == "__main__":
