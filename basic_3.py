b44697b32eccdf2f0a6ae2736afaedf7205c8796
Shrey Shah <shreybshah.97.ss@gmail.com>
2022-10-19 13:45:41 -0700

    Correct the dp array declaration

--- basic_3.py
+++ basic_3.py
@@ -2,39 +2,22 @@ from ctypes import alignment
 import sys
 import time
 import psutil
-import json
 
 dict = {"A":0, "C":1, "G":2, "T":3}
 def process_memory():
     process = psutil.Process()
     memory_info = process.memory_info()
     memory_consumed = int(memory_info.rss/1024)
+    print(memory_consumed)
     return memory_consumed
-
-def updateMetrics(file_name, memory_consumed, time_taken, algorithm_used, input_count):
-    metric_data = {}
-    with open(file_name, 'r') as f:
-        metric_data = json.loads(f.read())
-        f.close()
-    metric_data[algorithm_used]['memory_consumed'].insert(input_count-1,  memory_consumed)
-    metric_data[algorithm_used]['time_taken'].insert(input_count-1,  time_taken)
-    json_object = json.dumps(metric_data, indent=4)
-    with open(file_name, 'w') as f:
-        f.write(json_object)
-        f.close()
 
 def time_wrapper(driver_func):
     start_time = time.time()
-    data = driver_func()
+    driver_func()
     end_time = time.time()
     time_taken = (end_time - start_time)*1000
-<<<<<<< HEAD
-    memory_consumed= process_memory()
-    updateMetrics(data[0], memory_consumed, time_taken, data[1], data[2])
-=======
     print(time_taken)
     process_memory()
->>>>>>> 31613d1 (Change the location of memory calculate call)
     return time_taken
 
 def read_file(input_file):
@@ -113,6 +96,8 @@ def calculate_cost(generated_strings):
                 dp[i-1][j-1] + ALPHA[dict[string1[i-1]]][dict[string2[j-1]]], 
                 DELTA + dp[i-1][j],
                 DELTA + dp[i][j-1])
+
+    print(dp[string1_len][string2_len])
     return dp
 
 def calculate_alignment(generated_strings, dp):
@@ -120,8 +105,6 @@ def calculate_alignment(generated_strings, dp):
     Returns the aligned string for string1 and string2
     """
     string1, string2 = generated_strings
-<<<<<<< HEAD
-<<<<<<< HEAD
     i = len(string1)
     j = len(string2)
     aligned_string_1 = ""
@@ -158,78 +141,6 @@ def verify_cost(aligned_strings, dp):
     aligned_string_1, aligned_string_2 = aligned_strings
     aligned_string_len = len(aligned_string_1)
     cost=0
-
-    for i in range(aligned_string_len):
-        if(aligned_string_1[i] == '_' or aligned_string_2[i] == '_'):
-            cost += DELTA
-        else:
-            cost += ALPHA[dict[aligned_string_1[i]]][dict[aligned_string_2[i]]]
-    
-    if dp[-1][-1] != cost:
-        print("Minimum cost calculated does not match with the aligned sequence costs")
-        return -1
-    return cost
-
-
-def driver():
-    input_file_path = sys.argv[1]
-    input_file_count = int(sys.argv[2]) 
-    output_file_path = sys.argv[3]
-    output_metric_file_path = sys.argv[4]
-    initialize_variables()
-    lines = read_file(input_file_path)
-    generated_strings, base_lengths, operation_counts = generate_strings(lines)
-    validate_strings(generated_strings, base_lengths, operation_counts)
-    dp = calculate_cost(generated_strings)
-    aligned_strings = calculate_alignment(generated_strings, dp)
-    verify_cost(aligned_strings, dp)
-    return (output_metric_file_path, 'basic', input_file_count)
-
-def main():
-    time_wrapper(driver)
-=======
-    string1_len = len(string1)
-    string2_len = len(string2)
-=======
-    i = len(string1)
-    j = len(string2)
->>>>>>> a96bf1f (Add code for sequence alignment and memory and time calculations)
-    dict = {"A":0, "C":1, "G":2, "T":3}
-    aligned_string_1 = ""
-    aligned_string_2 = ""
-    
-    while i!=0 and j!=0:
-        if(dp[i][j] == dp[i-1][j-1] + ALPHA[dict[string1[i-1]]][dict[string2[j-1]]]):
-            aligned_string_1 = string1[i-1] + aligned_string_1
-            aligned_string_2 = string2[j-1] + aligned_string_2
-            i-=1
-            j-=1
-        elif(dp[i][j] == DELTA + dp[i][j-1]):
-            aligned_string_1 = "_" + aligned_string_1
-            aligned_string_2 = string2[j-1] + aligned_string_2
-            j-=1
-        else:
-            aligned_string_1 = string1[i-1] + aligned_string_1
-            aligned_string_2 = "_" + aligned_string_2
-            i-=1
-    
-    while i!=0:
-        aligned_string_1 = string1[i-1] + aligned_string_1
-        aligned_string_2 = "_" + aligned_string_2
-        i-=1
-    
-    while j!=0:
-        aligned_string_1 = "_" + aligned_string_1
-        aligned_string_2 = string2[j-1] + aligned_string_2
-        j-=1
-
-    return [aligned_string_1, aligned_string_2]
-
-def verify_cost(aligned_strings, dp):
-    aligned_string_1, aligned_string_2 = aligned_strings
-    aligned_string_len = len(aligned_string_1)
-    cost=0
-    dict = {"A":0, "C":1, "G":2, "T":3}
 
     for i in range(aligned_string_len):
         if(aligned_string_1[i] == '_' or aligned_string_2[i] == '_'):
@@ -256,7 +167,6 @@ def driver():
     print(aligned_strings[0]+"\n"+aligned_strings[1])
     verify_cost(aligned_strings, dp)
     return
->>>>>>> 3233ddc (Code refactoring)
 
 def main():
     time_wrapper(driver)
